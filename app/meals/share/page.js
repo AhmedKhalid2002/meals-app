eeccc8bf8b1fa150a829c53e72708949fabd5024
AhmedKhalid2002 <kmalahmdkhald927@gmail.com>
2026-03-10 14:09:22 +0200

    image picker

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,4 @@
+import ImagePacker from '@/components/meals/Image-packer';
 import classes from './page.module.css'
 
 export default function ShareMealPage() {
@@ -38,7 +39,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePacker/>
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
