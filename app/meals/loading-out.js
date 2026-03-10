f4a4d40b784b2061c3e4d77d9db331718098081c
AhmedKhalid2002 <kmalahmdkhald927@gmail.com>
2026-03-10 10:21:24 +0200

    add error file

--- app/meals/loading-out.js
+++ app/meals/loading-out.js
@@ -1,4 +1,4 @@
-import classes from './loading.module.css'
+import classes from './loading-out.module.css'
 export default function MealsLoading() {
   return (
     <div className={classes.loading}>Loading</div>
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,15 @@
 import Link from 'next/link';
-
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+import MealsLoading from './loading-out';
 
-export default async function MealsPage() {
-  const meals=await getMeals()
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -17,13 +21,13 @@ export default async function MealsPage() {
           Choose your favorite recipe and cook it yourself. It is easy and fun!
         </p>
         <p className={classes.cta}>
-          <Link href="/meals/share">
-            Share Your Favorite Recipe
-          </Link>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<MealsLoading/>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
