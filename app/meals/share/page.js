d266dc92663cd5993d02ff250f8b4fc2eb2f2c07
AhmedKhalid2002 <kmalahmdkhald927@gmail.com>
2026-03-12 12:34:08 +0200

    insert database

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,10 @@
+'use client'
 import ImagePacker from '@/components/meals/Image-packer';
-import classes from './page.module.css'
+import classes from './page.module.css';
+import { shareImage } from '@/lib/actions';
 
-export default function ShareMealPage() {
+export default  function ShareMealPage() {
+  
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +14,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareImage}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +42,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePacker/>
+          <ImagePacker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
